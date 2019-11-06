80e082efd35fd9cfe2e81c096b8d97677245b1b6
manikanth.erukulla <manikanth.erukulla@sohohouse.com>
2019-11-06 13:52:47 +0000

    changes to the ios pack

--- native_mobile/pageobjects/onboarding_objects.rb
+++ native_mobile/pageobjects/onboarding_objects.rb
@@ -70,7 +70,7 @@ class Ios_Onboarding_Objects
     @driver.find_element(:name => "OK")
   end
 
-  def you_are_set_text_2
+  def you_are_set_text
     @driver.find_element(:name => "You're all set")
   end
 
